c997ef2ac309fd1a85a499682d9f46b2077204e7
Flavio Mattos <fmattos@hyperwallet.com>
2019-05-03 15:19:03 -0700

    add PayPal get test to Hyperwallet test suite

--- core/src/test/java/com/hyperwallet/android/HyperwalletTestSuite.java
+++ core/src/test/java/com/hyperwallet/android/HyperwalletTestSuite.java
@@ -9,6 +9,7 @@ import com.hyperwallet.android.transfermethod.HyperwalletDeactivateBankCardTest;
 import com.hyperwallet.android.transfermethod.HyperwalletDeactivatePayPalAccountTest;
 import com.hyperwallet.android.transfermethod.HyperwalletGetBankAccountTest;
 import com.hyperwallet.android.transfermethod.HyperwalletGetBankCardTest;
+import com.hyperwallet.android.transfermethod.HyperwalletGetPayPalTest;
 import com.hyperwallet.android.transfermethod.HyperwalletListBankAccountsTest;
 import com.hyperwallet.android.transfermethod.HyperwalletListBankCardsTest;
 import com.hyperwallet.android.transfermethod.HyperwalletListTransferMethodsTest;
@@ -29,6 +30,7 @@ import org.junit.runners.Suite;
         HyperwalletCreatePayPalAccountTest.class,
         HyperwalletGetBankAccountTest.class,
         HyperwalletGetBankCardTest.class,
+        HyperwalletGetPayPalTest.class,
         HyperwalletUpdateBankAccountTest.class,
         HyperwalletUpdateBankCardTest.class,
         HyperwalletUpdatePayPalAccountTest.class,
