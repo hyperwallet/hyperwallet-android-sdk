82bdfecaf7e00a5b64a5bc86a11be9a3c21fdd76
azakrevska-epam <Anna_Zakrevska@epam.com>
2019-05-29 15:02:57 +0300

    HW-53066. Receipt query params

--- core/src/main/java/com/hyperwallet/android/model/receipt/Receipt.java
+++ core/src/main/java/com/hyperwallet/android/model/receipt/Receipt.java
@@ -48,7 +48,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Represents the receipt fields
+ * Represents the receipt mFields
  */
 public final class Receipt implements HyperwalletJsonModel, Parcelable {
 
