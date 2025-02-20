e617060e751eefaf4cb41a9b6058182f30ebdc85
Gustavo Meyer <grmeyer@paypal.com>
2025-02-20 10:15:04 -0500

    Update robolectric and hamcrest

--- core/src/test/java/com/hyperwallet/android/util/MockShadowSystemClock.java
+++ core/src/test/java/com/hyperwallet/android/util/MockShadowSystemClock.java
@@ -5,7 +5,10 @@ import android.os.SystemClock;
 import org.robolectric.annotation.Implements;
 
 /**
- *
+ * Simple SystemClock Shadow representation
+ * <p>
+ * Since Gradle 7, the Shadows SystemClock no longer supported
+ * by Android Shadow provided by Robolectric
  */
 @Implements(value = SystemClock.class, callThroughByDefault = true)
 public final class MockShadowSystemClock {
